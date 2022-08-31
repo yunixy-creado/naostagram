03d6332d7b1d9cb7202043ef9edf9e978de4d224
naopandao <nao_yoshida@yunixy.co.jp>
2022-08-31 22:35:30 +0900

    YX-227 投稿の詳細ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
   get '/users/:id', to: 'users#show', as: 'user'
-  resources :posts, only: %i(new create index) do
+  resources :posts, only: %i(nindex new create show) do
     resources :photos, only: %i(create)
   end
   
