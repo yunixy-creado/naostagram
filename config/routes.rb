0c5468e38b8917565d050bf1772697b298bb56b8
naopandao <nao_yoshida@yunixy.co.jp>
2022-09-01 00:12:52 +0900

    YX-227 投稿の削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
   get '/users/:id', to: 'users#show', as: 'user'
-  resources :posts, only: %i(nindex new create show) do
+  resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
   end
   
