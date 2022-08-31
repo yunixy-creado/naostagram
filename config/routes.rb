bda260fe62a627f7cc6fa484c8c9d46d3582a946
naopandao <nao_yoshida@yunixy.co.jp>
2022-09-01 01:42:38 +0900

    YX-227 いいね機能の実装。いいね反映の遅延あるので後で検証。

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'users#show', as: 'user'
   resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
+    resources :likes, only: %i(create destroy)
   end
   
 end
