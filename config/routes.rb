8f2415cce25f1baa105b4b75da1db3d34a25a705
naopandao <nao_yoshida@yunixy.co.jp>
2022-09-01 02:21:10 +0900

    YX-227 コメント機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
     resources :likes, only: %i(create destroy)
+    resources :comments, only: %i(create destroy)
+
   end
   
 end
